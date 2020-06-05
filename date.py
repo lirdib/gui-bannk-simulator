a5386ef02d495663cf1e315ad1136481c5df3225
lirdi <38312870+lirdib@users.noreply.github.com>
2020-06-05 10:49:46 +0200

    chekc_for_file function moved to func
    date is updated with check_for_file function moved to func file due to a circular inport error

--- date.py
+++ date.py
@@ -1,5 +1,4 @@
 from datetime import datetime as dt
-import func
 import os.path
 import investment
 def write_date(a,n):
@@ -28,23 +27,3 @@ def read_date():
         f.close()
     else:
         print("error")
-
-name_one = func.name+ ",dateone.txt"
-name_two =  func.name+ ",datesix.txt"
-name_three= func.name+ ",datetwelve.txt"
-def chekck_for_file(a,type):
-    if os.path.isfile(number_one)or os.path.isfile(number_two) or os.path.isfile(number_three):
-        func.destroy(a)
-        investment.invest(a)
-    else:
-        if type=='o':
-            n = func.name + ",dateone.txt"
-            write_date(a, n, type)
-        if type=='s':
-            n = func.name + ",datesix.txt"
-            write_date(a, n, type)
-        if type=='t':
-            n = func.name + ",datetwelve.txt"
-            write_date(a, n, type)
-        else:
-            func.inner_menu(a)
